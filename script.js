729528c8aef8e1f9bdf54cc5b10834148384113c
Sergey <moshko.2015@mail.ru>
2021-03-04 16:15:40 -0800

    Second task

--- script.js
+++ script.js
@@ -8,12 +8,34 @@ const personalMoviesDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+    
+/*for(let i=1; i<=2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?','');
+    const b = prompt('На сколько оцените его?','');
+    personalMoviesDB.movies[a] = b;
+}*/
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+let i=1;
+while(i <= 2) {
+
+    const a = prompt('Один из последних просмотренных фильмов?','');
+    const b = prompt('На сколько оцените его?','');
+
+    if(a == "" || b == "" || a.lenght > 50 || a == null || b == null) {
+            continue;
+    }
+    personalMoviesDB.movies[a] = b;
+    i++;
+}
+
+if(personalMoviesDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов!");
+} else if(personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log("Вы классический зритель.");
+} else if(personalMoviesDB.count >= 50) {
+    console.log("Вы киноман!");
+} else {
+    console.log("Произошла ошибка...");
+}
 
 console.log(personalMoviesDB);
